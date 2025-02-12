4bee3662c68212451d24b4a3f4619afb7728b384
Parth1185 <156308831+Parth1185@users.noreply.github.com>
2025-02-12 11:36:44 +0530

    Time: 0 ms (100%), Space: 62.9 MB (70.22%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,23 +1,40 @@
+// class Solution {
+// public:
+//     int maxArea(vector<int>& height) {
+//         int l=0, r = height.size()-1, ans = 0;
+//         while(l<r){
+
+//             if(height[l]<height[r]){
+//                 ans = max(ans, (r-l)*height[l]);
+//                 l++;
+//             }
+
+//             else{
+//                 ans = max(ans, (r-l)*height[r]);
+//                 r--;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int ans = 0 ;   
-        int l = 0 ; 
-        int r =  height.size() -1 ; 
-
-        while(l < r){
-
-            if (height[l] < height[r])
-                {
-                    ans=max(ans,((r-l)*height[l]));
-                    l++;
-                }            
-            else
-            {
-                ans=max(ans,((r-l)*height[r]));
-                r-- ; 
+        int ans = 0;
+        for (int l = 0, r = height.size() - 1; l < r; ) {
+            ans = max(ans, (r - l) * min(height[l], height[r]));
+            
+            if (height[l] < height[r]) {
+                l++;  
+            } else {
+                r--;  
             }
         }
-        return ans ; 
+        return ans;
     }
 };
