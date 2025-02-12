f9c07618d9b8ef2985868f7bd3899e7927709b79
Parth1185 <156308831+Parth1185@users.noreply.github.com>
2025-02-12 11:02:20 +0530

    Time: 0 ms (100%), Space: 13.7 MB (49.24%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,29 +1,57 @@
+// class Solution {
+// public:
+//     int compress(vector<char>& chars) {
+//         string s;
+//         int count = 1;
+        
+//         for(int i = 0; i < chars.size(); i++) {
+            
+//             if (i + 1 < chars.size() && chars[i] == chars[i + 1]) {
+//                 count++;
+//             } else {
+            
+//                 s += chars[i];
+
+            
+//                 if (count > 1) {
+//                     s += to_string(count);
+//                 }
+//                 count = 1; 
+//             }
+//         }
+
+//         for (int i = 0; i < s.size(); i++) {
+//             chars[i] = s[i];
+//         }
+
+//         return s.size(); 
+//     }
+// };
+
+
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        string s;
-        int count = 1;
-        
-        for(int i = 0; i < chars.size(); i++) {
-            
-            if (i + 1 < chars.size() && chars[i] == chars[i + 1]) {
-                count++;
-            } else {
-            
-                s += chars[i];
+        int write = 0, read = 0;
 
-            
-                if (count > 1) {
-                    s += to_string(count);
+        while (read < chars.size()) {
+            char currentChar = chars[read];
+            int count = 0;
+
+            while (read < chars.size() && chars[read] == currentChar) {
+                count++;
+                read++;
+            }
+
+            chars[write++] = currentChar;
+
+            if (count > 1) {
+                string countStr = to_string(count);
+                for (char c : countStr) {
+                    chars[write++] = c;
                 }
-                count = 1; 
             }
         }
-
-        for (int i = 0; i < s.size(); i++) {
-            chars[i] = s[i];
-        }
-
-        return s.size(); 
+        return write;
     }
 };
