59fad71564a6bb2af96dfa2b2827cdc9403ce211
Parth1185 <156308831+Parth1185@users.noreply.github.com>
2025-03-17 19:15:35 +0530

    Time: 7 ms (98.18%), Space: 60.4 MB (59.77%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,19 +1,38 @@
-import java.util.Stack;
+// import java.util.Stack;
+// class Solution {
+//     public int[] dailyTemperatures(int[] temperatures) {
+//         Stack <Integer> warm = new Stack<>();
+//         int n = temperatures.length;
+//         int[] answer = new int[n];
+//         for(int i=0; i<n; i++){
+//             while(!warm.isEmpty() && temperatures[i]>temperatures[warm.peek()]){
+//                 int index = warm.pop();
+//                 answer[index] = i - index;
+//             }
+//         warm.push(i);
+//         }
+//         return answer;
+//     }
+// }
 
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
-        int[] answer = new int[n]; 
-        Stack<Integer> warm = new Stack<>(); 
-
-        for (int i = 0; i < n; i++) {
-            while (!warm.isEmpty() && temperatures[i] > temperatures[warm.peek()]) {
-                int index = warm.pop(); 
-                answer[index] = i - index; 
-            }
-            warm.push(i);
-        }
+        int[] answer = new int[n];
+        int hottest = 0; // Track the hottest temperature seen from the right
         
+        for (int i = n - 1; i >= 0; i--) { 
+            if (temperatures[i] >= hottest) {
+                hottest = temperatures[i]; 
+                answer[i] = 0; // No warmer day ahead
+            } else {
+                int days = 1;
+                while (temperatures[i + days] <= temperatures[i]) {
+                    days += answer[i + days]; // Skip ahead using answer array
+                }
+                answer[i] = days;
+            }
+        }
         return answer;
     }
 }
