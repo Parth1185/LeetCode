6c6a85999056816b37cee970e5ec441a46c3e547
Parth1185 <156308831+Parth1185@users.noreply.github.com>
2025-03-17 18:19:10 +0530

    Time: 2 ms (97.63%), Space: 41.8 MB (65.83%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -25,6 +25,37 @@
 // }
 
 
+// import java.util.Stack;
+
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> stack = new Stack<>();
+
+//         for (int i = 0; i < s.length(); i++) {
+//             char c = s.charAt(i);
+
+//             if (c == '(' || c == '{' || c == '[') {
+//                 stack.push(c);
+//             } 
+//             else {
+//                 if (stack.isEmpty()) return false; // Prevent stack underflow
+                
+//                 char top = stack.peek();
+//                 if ((c == ')' && top == '(') || 
+//                     (c == '}' && top == '{') || 
+//                     (c == ']' && top == '[')) {
+//                     stack.pop();
+//                 } 
+//                 else {
+//                     return false;
+//                 }
+//             }
+//         }
+
+//         return stack.isEmpty();
+//     }
+// }
+
 import java.util.Stack;
 
 class Solution {
@@ -37,18 +68,14 @@ class Solution {
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
             } 
+            else if (!stack.isEmpty() && 
+                    ((c == ')' && stack.peek() == '(') || 
+                     (c == '}' && stack.peek() == '{') || 
+                     (c == ']' && stack.peek() == '['))) {
+                stack.pop();
+            } 
             else {
-                if (stack.isEmpty()) return false; // Prevent stack underflow
-                
-                char top = stack.peek();
-                if ((c == ')' && top == '(') || 
-                    (c == '}' && top == '{') || 
-                    (c == ']' && top == '[')) {
-                    stack.pop();
-                } 
-                else {
-                    return false;
-                }
+                return false;
             }
         }
 
